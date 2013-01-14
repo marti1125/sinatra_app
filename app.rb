3deda6a5efac4c02ac1e9fce4d7e8ce530aea85a
Willy Aguirre <marti1125@gmail.com>
2013-01-14 11:17:08 -0500

    routing

--- app.rb
+++ app.rb
@@ -4,4 +4,20 @@ class App < Sinatra::Base
 	get '/' do
 		"Hello World"
 	end
+	
+	post '/' do
+		"Hello World via POST!"
+	end
+	
+	put '/' do
+		"Hello World via PUT!"
+	end
+	
+	delete '/' do
+		"Goodbye World via DELETE!"
+	end
+	
+	get "hello/:first_name/?:last_name" do |first,last|
+		"Hello #{first} #{last}"
+	end
 end
